835a9de49e21936f59447a7e9c8d1f708be0a8d7
MEDINA Enzo <emedina@enseirb-matmeca.fr>
2021-02-03 16:55:45 +0100

    question 2

--- partie1.py
+++ partie1.py
@@ -29,6 +29,19 @@ def rp(x, p):
     if (x > 1):  return round(x, p-test)
     return  x
 
+def prec_add(x, y, p):
+    v1 = rp(x, p)
+    v2 = rp(y, p)
+
+    return (v1 + v2)
+
+def prec_prod(x, y, p):
+    v1 = rp(x, p)
+    v2 = rp(y, p)
+
+    return (v1 * v2)
+
+
 value = rp(10.235673, 6)
 print(value)
 
