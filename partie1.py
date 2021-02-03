0619d3838879a4e617b00f47f331f48a5f3023d4
MEDINA Enzo <emedina@enseirb-matmeca.fr>
2021-02-03 16:29:54 +0100

    first function

--- partie1.py
+++ partie1.py
@@ -1,1 +1,35 @@
+import math
+
 print("PARTIE 1")
+"""
+def test(x, p):
+    while (x > pow(10, p)):
+        x /= 10
+    x = round(x)
+    return x"""
+
+
+def rp(x, p):
+    exposant = 0
+    while x > 1:
+        x /= 10
+        exposant += 1
+
+    test=exposant
+
+    while x < 0.1:
+        x *= 10
+        exposant -= 1
+
+    #entier
+    x *= pow(10, p)
+    x = round(x)
+    x *= pow(10, exposant-p)
+
+    if (x > 1):  return round(x, p-test)
+    return  x
+
+value = rp(10.235673, 6)
+print(value)
+
+
