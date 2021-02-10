4e418e4c29b3cb96e57a6550ec519b61f85aa74a
MEDINA Enzo <emedina@enseirb-matmeca.fr>
2021-02-10 15:57:51 +0100

    fin partie 1 code

--- partie1.py
+++ partie1.py
@@ -5,23 +5,19 @@ import matplotlib.pyplot as p
 
 
 print("PARTIE 1")
-"""
-def test(x, p):
-    while (x > pow(10, p)):
-        x /= 10
-    x = round(x)
-    return x"""
-
 
 def rp(x, p):
+
+    if ( x== 0):   return 0;
+
     exposant = 0
-    while x > 1:
+    while x > 1 or x < -1:
         x /= 10
         exposant += 1
 
     test=exposant
 
-    while x < 0.1:
+    while x < 0.1 and x > -0.1:
         x *= 10
         exposant -= 1
 
@@ -48,6 +44,7 @@ def prec_prod(x, y, p):
 def erreur_add(x, y, p):
     real = x+y
     computer = prec_add(x, y, p)
+    
 
     return abs( (real - computer) / real )
 
@@ -59,14 +56,10 @@ def erreur_prod(x, y, p):
 
 def calcul_log(prec):
     sum = 0
-    for i in range(1, prec):
-        sum += pow(-1, i+1) / i
+    for i in range(1, pow(10, prec), 2):
+        sum += 1/(i*(i+1))
     return sum
 
-
-prec = 4
-value = calcul_log(prec)
-print(value)
 
 """
 x = 3.333
@@ -83,12 +76,55 @@ s = erreur_add(x, y, p)
 print(s)
 """
 
+
+# Valeurs intéressantes: 
 """
-x=n.linspace(-10,10,100)
+lx =n.linspace(-10,10,100)  ou 200
 y = 5
 prec = 3
-p.plot(x,prec_add(x, y, prec))  # on utilise la fonction sinus de Numpy
+
+y=0 special ?! 
+
+"""
+
+"""
+lx =n.linspace(-10,10,100)
+y = 5.75
+prec = 3
+
+
+fx = [erreur_add(x, y, prec) for x in lx]
+
+
+
+p.plot(lx, fx)  # on utilise la fonction sinus de Numpy
 p.ylabel('fonction sinus')
 p.xlabel("l'axe des abcisses")
-p.show()    
-"""
+p.show()  """
+
+
+print()
+
+
+# Question 5
+test = 7
+
+    # Erreur relative
+for i in range(1, 7):
+    prec = i
+    value = erreur_add(calcul_log(prec), n.log(2), prec)
+    print("Erreur relative avec une precision " + str(prec) + " : " + str(value))
+
+# Affichage de la précision du calcul de log en comparaison avec la vraie valeur de log
+
+value = calcul_log(prec)
+
+lx = [i for i in range(1, prec)]
+
+
+p.plot(lx, [calcul_log(prec) for prec in lx])  # on utilise la fonction sinus de Numpy
+p.plot(lx, [n.log(2) for x in lx])
+p.yscale("log")
+p.ylabel("l'axe des ordonnées")
+p.xlabel("l'axe des abcisses")
+p.show()  
