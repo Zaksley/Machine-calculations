95626a7167fdf57734a0c46f3d16e06207f8042a
KAMGANG NINTCHEU David <David.Kamgang_Nintcheu@bordeaux-inp.fr>
2021-02-10 20:25:54 +0100

    completion cordi

--- 2-part/cordic.py
+++ 2-part/cordic.py
@@ -4,7 +4,7 @@ ln10 = 2.3025850929940456840179914546843642076011014886287729760333279009
 
 import numpy as np
 
-A=[np.arctan(2^-k) for k in range(5)]
+A=[np.arctan(2**-k) for k in range(5)]
 
 def cordic_ln(x) :
 
@@ -62,21 +62,71 @@ def exp(x) :
 #403.42879349273403
 #403.4287934926897
 
-def cordic_tan(x) :
+import numpy as np
 
-    k = 0
-    n = 0
-    d = 1
+A=[np.arctan(2**(-k)) for k in range(5)]
 
-    while (k <= 4) :
-        while ( x >= A[k] ) :
-            x = x - A[k]
-            np = n + d*pow(10, -k)
-            d = d - n*pow(10, -k)
-            n = np
+def arctan(x):
+    k=0
+    y=1
+    r=0
+    x_1=x
+    if (x<0):
+        x=-x
+    elif(x>1):
+        x=1/x
+    while(k<=4):
+        while(x<y*(2**(-k))):
+            k=k+1
+        if (k<5):
+            xp=x-y*(2**(-k))
+            y=y+x*(2**(-k))
+            x=xp
+            r=r+A[k]
+    if (x_1<0):
+        return -(r+(x/y))
+    if(x_1>1):
+        return (np.pi/2)-(r+(x/y))
+    return (r+(x/y))
+print("artan(10)",int(arctan(1)*10**12))
+print(int(np.arctan(1)*10**12)-int(arctan(1)*10**12))
 
-        k = k + 1
+T_arctan=[]
+for i in range(1000):
+    T_arctan.append(int(np.arctan(i)*10**12)-int(arctan(i)*10**12))
+print(T_arctan)
 
-    return (n + x*d)/(d - x*n)
 
-print(cordic_tan(3))
+def tan(x):
+    k=0
+    n=0
+    d=1
+    x_1=x
+    if ((x>(np.pi/4)) and (x<(np.pi/2))):
+        x=(np.pi/2)-x
+    if (x>(np.pi/2)):
+        while (x>np.pi):
+            x=x-np.pi
+        x=np.pi-x
+    while(k<=4):
+        while(x>=A[k]):
+            x=x-A[k]
+            mp=n+d*(2**(-k))
+            d=d-n*(2**(-k))
+            n=mp
+        k=k+1
+    if ((x_1>(np.pi/4)) and (x_1<(np.pi/2))):
+    	return 1/((n+x*d)/(d-x*n))
+    if (x_1>(np.pi/2)):
+    	return -(n+x*d)/(d-x*n)
+
+    return (n+x*d)/(d-x*n)
+print("pour tant")
+print(tan(1))
+print(np.tan(1))
+
+T_tan=[]
+for i in range(1000):
+    T_tan.append(int(np.tan(i)*10**12)-int(tan(i)*10**12))
+print(T_tan)
+
